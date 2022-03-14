ee5424f6a57e9625235061db0aa6892227b8fc18
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-14 12:36:32 +0000

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
 import './App.css';
+import { Navbar } from './layout'
+import { Home } from './pages'
+
 
 function App() {
   return (
     <div className="App">
-        <h1>App</h1>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          {/* <Route path="/create-lobby">
+            <Create />
+          </Route>
+          <Route path="/join-lobby">
+            <Join />
+          </Route>
+          <Route path="/leaderboard">
+            <Leaderboard />
+          </Route> */}
+        </Switch>
     </div>
   );
 }
