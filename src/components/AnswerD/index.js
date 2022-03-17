50ebcf5cebf299f97e1495b35d1d00a067436eff
bojin11 <15011278723@163.com>
2022-03-17 20:52:34 +0000

    make changes to the quiz page to adapt to the api changes

--- src/components/AnswerD/index.js
+++ src/components/AnswerD/index.js
@@ -3,10 +3,8 @@ import React, { Component } from "react";
 class AnswerD extends Component {
 
     render() {
-        
-        var parser = new DOMParser();
-        var dom = parser.parseFromString('<!doctype html><body>' + this.props.answerText, 'text/html');
-        var decodedText = dom.body.textContent;
+
+        var decodedText = decodeURIComponent(this.props.answerText);
 
         return (
     
