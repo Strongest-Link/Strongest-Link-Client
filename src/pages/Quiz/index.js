31888186ef1d665d051097659825d221844c65ac
r0neel <ronbhagwakar@gmail.com>
2022-03-18 04:10:24 +0000

    Update Quiz page to pull from server and use sockets

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import Question from "../../components/Question";
 import AnswerA from "../../components/AnswerA";
@@ -8,155 +7,115 @@ import AnswerC from "../../components/AnswerC";
 import AnswerD from "../../components/AnswerD";
 import QuestionProgressBar from "../../components/QuestionProgressBar";
 
-import { useParams } from "react-router-dom";
-
-function Quiz({ gameData }) {
-  // let { category } = useParams();
-  // let { difficulty } = useParams();
-  let { numberOfQuestions } = gameData.questions.length;
-
-  // //CONSTANTS
-  const CORRECT_BONUS = 10;
-
-  // &nbsp; is the text encoding for a space character. Added one to each default state to prove our decoding is working.
+function Quiz({ setGame, gameData, socket }) {
+  // // &nbsp; is the text encoding for a space character. Added one to each default state to prove our decoding is working.
   const [theQuestion, setTheQuestion] = useState("Question&nbsp;...");
   const [choiceA, setChoiceA] = useState("Answer&nbsp;A ...");
   const [choiceB, setChoiceB] = useState("Answer&nbsp;B ...");
   const [choiceC, setChoiceC] = useState("Answer&nbsp;C ...");
   const [choiceD, setChoiceD] = useState("Answer&nbsp;D ...");
+  const [score, setScore] = useState(0);
 
-  useEffect(() => {
-    let questionCounter = 0;
-
-    const choiceTextElements = document.getElementsByClassName("choice-text");
-    const choices = [];
-
-    if (choiceTextElements.length === 4) {
-      choices[0] = ReactDOM.findDOMNode(choiceTextElements[0]);
-      choices[1] = ReactDOM.findDOMNode(choiceTextElements[1]);
-      choices[2] = ReactDOM.findDOMNode(choiceTextElements[2]);
-      choices[3] = ReactDOM.findDOMNode(choiceTextElements[3]);
-    }
-
-    const scoreElement = document.getElementById("score");
-    const scoreText = ReactDOM.findDOMNode(scoreElement);
-
-    let currentQuestion = {};
-    let acceptingAnswers = false;
-    let score = 0;
-    let availableQuestions = [];
-
-    let questions = gameData.questions.map((loadedQuestion) => {
-      const formattedQuestion = {
-        question: loadedQuestion.question
-      };
-
-      const answerChoices = [...loadedQuestion.incorrect_answers];
-      formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-      answerChoices.splice(
-        formattedQuestion.answer - 1,
-        0,
-        loadedQuestion.correct_answer
-      );
-
-      answerChoices.forEach((choice, index) => {
-        formattedQuestion["choice" + (index + 1)] = choice;
-      });
-
-      return formattedQuestion;
-    });
-    // startGame();
-
-    // function startGame() {
-    //   questionCounter = 0;
-
-    //   score = 0;
-    //   availableQuestions = [...questions];
-
-    //   getNewQuestion();
-    // }
-
-    function getNewQuestion() {
-      if (questions.length === 0 || questionCounter >= numberOfQuestions) {
-        localStorage.setItem("mostRecentScore", score);
-        //go to the end page
-        // return window.location.assign("/Leaderboard");
-      }
-
-      questionCounter++;
-      // Update the progress bar using the DOM and not by setState so that we do not trigger useEffect infinite loop
-      const questionCounterElement = document.getElementById("questionCounter");
-      const progressBarFullElement = document.getElementById("progressBarFull");
-      questionCounterElement.innerText = questionCounter;
-      progressBarFullElement.style.width = `${
-        (questionCounter / numberOfQuestions) * 100
-      }%`;
-
-      const questionIndex = Math.floor(Math.random() * questions.length);
-      currentQuestion = questions[questionIndex];
-
-      setTheQuestion(currentQuestion.question);
-      console.log(currentQuestion);
-      setChoiceA(currentQuestion["choice1"]);
-      setChoiceB(currentQuestion["choice2"]);
-      setChoiceC(currentQuestion["choice3"]);
-      setChoiceD(currentQuestion["choice4"]);
-
-      questions.splice(questionIndex, 1);
-      acceptingAnswers = true;
-    }
-
-    choices.forEach((choice) => {
-      choice.addEventListener("click", (e) => {
-        if (!acceptingAnswers) return;
-
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-
-        // We need to use == here and not ===. We are only testing that they have the same value.
-        var isCorrectAnswer = selectedAnswer == currentQuestion.answer;
-
-        const classToApply = isCorrectAnswer ? "correct" : "incorrect";
-
-        if (classToApply === "correct") {
-          incrementScore(CORRECT_BONUS);
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply);
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const selectedChoice = e.target.innerText;
+    const element = e.target;
+    const payload = {
+      lobbyName: gameData.name,
+      roomId: gameData.id,
+      answer: selectedChoice
+    };
+    socket.emit("answer", payload);
+    await socket.on("response", (res) => {
+      if (res.response === "It's not your turn.") {
+        alert(res.response);
+      } else {
+        const correctAnswer = res.response.correct;
+        // const gameEnd = res.response.gameEnd;
+        const classToApply = correctAnswer ? "correct" : "incorrect";
+        element.classList.add(classToApply);
         setTimeout(() => {
-          selectedChoice.parentElement.classList.remove(classToApply);
-          getNewQuestion();
+          element.classList.remove(classToApply);
+          setGame(res.response.currentState);
         }, 1000);
-      });
+      }
     });
+  };
 
-    function incrementScore(num) {
-      score += num;
-      scoreText.innerText = score;
+  socket.on("next", (res) => {
+    setTimeout(() => {
+      setGame(res.response.currentState);
+    }, 1000);
+  });
+
+  let questions = [];
+  gameData.questions.map((question) => {
+    return questions.push(question);
+  });
+  let answerArray = [];
+  for (const q in questions) {
+    let array = [];
+    array.push(questions[q].correct_answer);
+    questions[q].incorrect_answers.map((answer) => {
+      return array.push(answer);
+    });
+    answerArray.push(array);
+  }
+  useEffect(() => {
+    const username = socket.username;
+    for (const score in gameData.scores) {
+      if (score === username) {
+        setScore(gameData.scores[score]);
+      }
     }
-  }, [numberOfQuestions]); // These properties must not change otherwise the fetching won't pause for each question
+
+    let currentQuestion = gameData.currentQuestion;
+    setTheQuestion(decodeURIComponent(questions[currentQuestion].question));
+    setChoiceA(decodeURIComponent(answerArray[currentQuestion][0]));
+    setChoiceB(decodeURIComponent(answerArray[currentQuestion][1]));
+    setChoiceC(decodeURIComponent(answerArray[currentQuestion][2]));
+    setChoiceD(decodeURIComponent(answerArray[currentQuestion][3]));
+
+    // Update the progress bar using the DOM and not by setState so that we do not trigger useEffect infinite loop
+    const questionCounterElement = document.getElementById("questionCounter");
+    const progressBarFullElement = document.getElementById("progressBarFull");
+    questionCounterElement.innerText = currentQuestion;
+
+    progressBarFullElement.style.width = `${
+      (currentQuestion / gameData.questions.length) * 100
+    }%`;
+  }, [gameData]);
 
   return (
     <div className="container">
       <div id="game" className="justify-center flex-column">
         <div id="hud">
           <div id="hud-item">
-            <QuestionProgressBar maxQuestions={numberOfQuestions} />
+            <QuestionProgressBar maxQuestions={gameData.questions.length} />
           </div>
           <div id="hud-item">
             <p className="hud-prefix">Score</p>
             <h1 className="hud-main-text" id="score">
-              0
+              {score}
             </h1>
           </div>
         </div>
         <Question questionText={theQuestion} />
-        <AnswerA answerText={choiceA} />
-        <AnswerB answerText={choiceB} />
-        <AnswerC answerText={choiceC} />
-        <AnswerD answerText={choiceD} />
+        <div onClick={handleSubmit}>
+          <AnswerA answerText={choiceA} />
+        </div>
+
+        <div onClick={handleSubmit}>
+          <AnswerB answerText={choiceB} />
+        </div>
+
+        <div onClick={handleSubmit}>
+          <AnswerC answerText={choiceC} />
+        </div>
+
+        <div onClick={handleSubmit}>
+          <AnswerD answerText={choiceD} />
+        </div>
       </div>
     </div>
   );
